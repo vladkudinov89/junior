ffa7fa3bdf2f3190682de50e5fe627ed73f3b70a
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-10-14 11:37:15 +0300

    Add api logic to app

---
+++ app/Mail/SendOrderMail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendOrderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+   private $order;
+    public function __construct(int $order)
+    {
+        $this->order = $order;
+    }
+
+    public function build()
+    {
+        return $this->view('email.order')
+            ->with([
+                'order' => $this->order
+            ]);
+    }
+}
